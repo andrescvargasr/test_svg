6071f01fe7bb5fbdbe24eba54db2509121642bb2
andrescvargasr <andresc.vargasr@gmail.com>
2023-03-01 16:05:29 -0500

    feat: animate MeasureRule

--- src/components/bioreactor/layouts/MeasureRule.jsx
+++ src/components/bioreactor/layouts/MeasureRule.jsx
@@ -3,14 +3,22 @@ import React from "react";
 const MeasureRule = () => {
   return (
     <>
-      <g id="Layer_2"></g>
       <polyline
         fill="#FFFFFF"
         stroke="#000000"
         strokeMiterlimit="10"
         points="139.235,215.938 127.301,215.938 127.301,392.299 
     139.235,392.299 "
-      ></polyline>
+      >
+        <animate
+          attributeName="points"
+          dur="5s"
+          repeatCount="indefinite"
+          from="139,215 127,215 127,392 139,392"
+          to="139,300 127,300 127,392 139,392"
+          values="139,215 127,215 127,392 139,392;139,300 127,300 127,392 139,392;139,215 127,215 127,392 139,392"
+        />
+      </polyline>
     </>
   );
 };
